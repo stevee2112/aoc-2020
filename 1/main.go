65c15dd211bbc15c6e270c0ffa910e98202cf659
Stephen Englert <stevee2112@gmail.com>
2020-12-01 09:11:38 -0500

    finish day 1

--- 1/main.go
+++ 1/main.go
@@ -29,18 +29,38 @@ func main() {
 		expenseIndex[expence]++
 	}
 
-	var part1Val1, part1Val2 int
+	const sum = 2020
+	var part1Val1, part1Val2, part2Val1, part2Val2, part2Val3 int
 
-	for expense, _ := range expenseIndex {
-		needed := 2020 - expense
+	// Part 1
+	_, part1Val1, part1Val2 = getSummers(sum, expenseIndex)
 
-		if _, ok := expenseIndex[needed]; ok {
-			part1Val1 = expense
-			part1Val2 = needed
-			break
+	// Part 2
+
+	found := false
+	for value, _ := range expenseIndex {
+		needed := sum - value
+
+		found, part2Val2, part2Val3 = getSummers(needed, expenseIndex)
+
+		if found {
+			part2Val1 = value
+			break;
 		}
 	}
 
 	fmt.Printf("Part 1: %d\n", part1Val1 * part1Val2)
-	//fmt.Printf("Part 2: %d\n", atBasement)
+	fmt.Printf("Part 2: %d\n", part2Val1 * part2Val2 * part2Val3)
+}
+
+func getSummers(sum int, values map[int]int) (bool, int, int) {
+	for value, _ := range values {
+		needed := sum - value
+
+		if _, ok := values[needed]; ok {
+			return true, value, needed
+		}
+	}
+
+	return false, 0, 0
 }
