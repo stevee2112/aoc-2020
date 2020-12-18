2ce4203fee16404d638a8375259ed54665b23b5a
Stephen Englert <stevee2112@gmail.com>
2020-12-18 16:48:16 -0500

    Finish day 14

--- 14/main.go
+++ 14/main.go
@@ -16,7 +16,7 @@ func main() {
 	// Get Data
 	_, file, _,  _ := runtime.Caller(0)
 
-	input, _ := os.Open(path.Dir(file) + "/example")
+	input, _ := os.Open(path.Dir(file) + "/input")
 
 	defer input.Close()
 	scanner := bufio.NewScanner(input)
@@ -24,6 +24,7 @@ func main() {
 	currentMask := ""
 
 	memPart1 := map[int]int{}
+	memPart2 := map[int]int{}
 	for scanner.Scan() {
 		inputStr := scanner.Text()
 
@@ -38,6 +39,10 @@ func main() {
 			value ,_ := strconv.Atoi(memParts[1])
 
 			memPart1[address] = applyMask(value, currentMask)
+
+			for _,realAddress := range applyMemoryDecoder(address, currentMask) {
+				memPart2[realAddress] = value
+			}
 		}
 	}
 
@@ -47,8 +52,15 @@ func main() {
 		sumPart1 += value
 	}
 
+	// Part 2
+	sumPart2 := 0
+	for _,value := range memPart2 {
+		sumPart2 += value
+	}
+
+
 	fmt.Printf("Part 1: %d\n", sumPart1)
-	fmt.Printf("Part 2: %d\n", 0)
+	fmt.Printf("Part 2: %d\n", sumPart2)
 }
 
 func applyMask(value int, mask string) int {
@@ -58,7 +70,7 @@ func applyMask(value int, mask string) int {
 	for i := len(runes)-1; i >= 0; i-- {
 		if string(mask[i]) == "1" {
 			value = setBit(value, uint(at))
-		}
+					}
 
 		if string(mask[i]) == "0" {
 			value = clearBit(value, uint(at))
@@ -68,6 +80,85 @@ func applyMask(value int, mask string) int {
 	}
 
 	return value
+}
+
+func applyMemoryDecoder(address int, mask string) []int {
+
+	numberBits := strconv.FormatInt(int64(address), 2)
+
+	stringMask := ""
+	runes := []rune(mask)
+	at := 0
+
+	addresses := []int{}
+	addressesBits := []string{}
+
+	for i := len(runes)-1; i >= 0; i-- {
+
+		bit := ""
+
+		if string(mask[i]) == "1" {
+			stringMask = fmt.Sprintf("%s%s", stringMask, "1")
+			bit = "1"
+		}
+
+		if string(mask[i]) == "0" {
+			if at < len(numberBits) {
+				bit = string(numberBits[len(numberBits) - 1 - at])
+				stringMask = fmt.Sprintf("%s%s", stringMask, string(numberBits[len(numberBits) - 1 - at]))
+			} else {
+				bit = "0"
+			}
+		}
+
+		if string(mask[i]) == "X" {
+			bit = "X"
+			stringMask = fmt.Sprintf("%s%s", stringMask, "X")
+		}
+
+		addressesBits = addBit(bit, addressesBits)
+
+		at++
+	}
+
+	for _, addressBit := range addressesBits {
+		address,_ := strconv.ParseInt(reverse(addressBit), 2, 64)
+		addresses = append(addresses, int(address))
+	}
+
+	return addresses
+}
+
+func addBit(bit string, addresses []string) []string {
+
+	newAddresses := []string{}
+
+	if bit == "0" {
+		if len(addresses) < 1 {
+			newAddresses = append(newAddresses, "0")
+		} else {
+			for _,address := range addresses {
+				newAddresses = append(newAddresses, fmt.Sprintf("%s%s", address, "0"))
+			}
+		}
+	}
+
+	if bit == "1" {
+		if len(addresses) < 1 {
+			newAddresses = append(newAddresses, "1")
+		} else {
+			for _,address := range addresses {
+				newAddresses = append(newAddresses, fmt.Sprintf("%s%s", address, "1"))
+			}
+		}
+	}
+
+	if bit == "X" {
+		newAddresses = append(newAddresses, addBit("0", addresses)...)
+		newAddresses = append(newAddresses, addBit("1", addresses)...)
+	}
+
+	return newAddresses
 }
 
 // Sets the bit at pos in the integer n.
@@ -81,4 +172,12 @@ func clearBit(n int, pos uint) int {
     mask := ^(1 << pos)
     n &= mask
     return n
+}
+
+func reverse(s string) string {
+    runes := []rune(s)
+    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+        runes[i], runes[j] = runes[j], runes[i]
+    }
+    return string(runes)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module stevee2112/aoc-2020
 
 go 1.15
 
-require github.com/GreenLightning/advent-of-code-downloader v1.0.0 // indirect
+require (
+	github.com/GreenLightning/advent-of-code-downloader v1.0.0 // indirect
+	github.com/golang-collections/go-datastructures v0.0.0-20150211160725-59788d5eb259
+	github.com/stretchr/testify v1.6.1 // indirect
+)
