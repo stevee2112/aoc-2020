20f764b0ac32fa03acb72f26026425bc0cdbf427
Stephen Englert <stevee2112@gmail.com>
2020-12-28 13:17:25 -0500

    Finish day 24

--- 24/main.go
+++ 24/main.go
@@ -57,8 +57,9 @@ func main() {
 		hexGrid.SetCoordinate(tile)
 	}
 
-	hexGrid.PrintWithFill(".")
+	hexGrid.Normalize()
 
+	// Part 1
 	sumBlack := 0
 	hexGrid.Iterate(func (c util.Coordinate) {
 		if c.Value == "B" {
@@ -66,8 +67,72 @@ func main() {
 		}
 	})
 
+	// Part 2
+	days := 100
+	for at := 0; at < days; at++ {
+		hexGrid = FlipFloor(hexGrid)
+	}
+
+	sumBlackPart2 := 0
+	hexGrid.Iterate(func (c util.Coordinate) {
+		if c.Value == "B" {
+			sumBlackPart2++
+		}
+	})
+
 	fmt.Printf("Part 1: %d\n", sumBlack)
-	fmt.Printf("Part 2: %d\n", 0)
+	fmt.Printf("Part 2: %d\n", sumBlackPart2)
+}
+
+func FlipFloor(hexGrid util.Grid) util.Grid {
+	newHexGrid := util.Grid{}
+	check := []util.Coordinate{}
+	// Get all Black Coordinates
+	hexGrid.Iterate(func (c util.Coordinate) {
+		if c.Value == "B" {
+			check = append(check, c)
+
+			// Get all coordinates adjacent to all black coordinates and combine with all black coordinates
+			// This is all the coordinates we need to check
+
+			check = append(check, GetAdjacent(c, hexGrid)...)
+		}
+	})
+
+	//for each apply rules and set value in NEW grid
+	for _,tile := range check {
+		newHexGrid.SetValue(tile.X, tile.Y, Flip(tile, hexGrid))
+	}
+
+	return newHexGrid
+}
+
+func Flip(at util.Coordinate, hexGrid util.Grid) string {
+
+	adjacent := GetAdjacent(at, hexGrid)
+	blkAdjCount := 0
+
+	for _,adj := range adjacent {
+		if adj.Value == "B" {
+			blkAdjCount++
+		}
+	}
+
+	if at.Value == "B" {
+		if blkAdjCount == 0 || blkAdjCount > 2 {
+			return "W"
+		}
+	} else { // W
+		if blkAdjCount == 2 {
+			return "B"
+		}
+	}
+
+	if at.Value == nil {
+		at.Value = "W"
+	}
+
+	return at.Value.(string)
 }
 
 func GetTileFromDirections(start util.Coordinate, directions []string, hexGrid util.Grid) util.Coordinate {
@@ -104,4 +169,61 @@ func GetTileFromDirections(start util.Coordinate, directions []string, hexGrid u
 	}
 
 	return finalAt
+}
+
+func GetAdjacent(at util.Coordinate, hexGrid util.Grid) []util.Coordinate {
+
+	adjacent := []util.Coordinate{}
+	x := at.X
+	y := at.Y
+
+	// e
+	east := hexGrid.GetCoordinate(x + 2, y)
+	if east.Value == nil {
+		east = util.Coordinate{x + 2, y, nil}
+	}
+
+	adjacent = append(adjacent, east)
+
+	// ne
+	northEast := hexGrid.GetCoordinate(x + 1, y - 1)
+	if northEast.Value == nil {
+		northEast = util.Coordinate{x + 1, y - 1, nil}
+	}
+
+	adjacent = append(adjacent, northEast)
+
+	// nw
+	northWest := hexGrid.GetCoordinate(x - 1, y - 1)
+	if northWest.Value == nil {
+		northWest = util.Coordinate{x - 1, y - 1, nil}
+	}
+
+	adjacent = append(adjacent, northWest)
+
+	// w
+	west := hexGrid.GetCoordinate(x - 2, y)
+	if west.Value == nil {
+		west = util.Coordinate{x - 2, y, nil}
+	}
+
+	adjacent = append(adjacent, west)
+
+	// sw
+	southWest := hexGrid.GetCoordinate(x - 1, y + 1)
+	if southWest.Value == nil {
+		southWest = util.Coordinate{x - 1, y + 1, nil}
+	}
+
+	adjacent = append(adjacent, southWest)
+
+	// se
+	southEast := hexGrid.GetCoordinate(x + 1, y + 1)
+	if southEast.Value == nil {
+		southEast = util.Coordinate{x + 1, y + 1, nil}
+	}
+
+	adjacent = append(adjacent, southEast)
+
+	return adjacent
 }
